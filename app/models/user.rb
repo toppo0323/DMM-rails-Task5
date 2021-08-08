a9f22a530539a6893119b3034da29dd36cd5284d
EC2 Default User <ec2-user@ip-172-31-19-194.ec2.internal>
2021-08-08 03:41:28 +0000

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,6 +37,20 @@ def favorite_by?(book_id)
 end
 
 
+def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+end
+
 
   attachment :profile_image
 
